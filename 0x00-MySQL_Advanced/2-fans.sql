d9032a96d8a3273be02e478e976e230dffdb0435
Simontagbor <simontagbor360@gmail.com>
2023-10-14 20:47:47 +0000

    fix script

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,9 +1,5 @@
 -- calculate band fans
-WITH FanCounts AS (
-  SELECT origin, SUM(nb_fans) AS total_fans
-  FROM metal_bands
-  GROUP BY origin
-)
-SELECT origin, total_fans, DENSE_RANK() OVER (ORDER BY total_fans DESC) AS country_rank
-FROM FanCounts
-ORDER BY total_fans DESC;
+SELECT origin, SUM(nb_fans) AS total_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY total_fans DESC
